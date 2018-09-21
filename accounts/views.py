a439eb98dd084ec278442f29c53d3cb1b106fb47
Jeferson <jppmainini@gmail.com>
2018-09-21 18:04:45 -0300

    .

--- accounts/views.py
+++ accounts/views.py
@@ -14,4 +14,11 @@ class RegisterView(LoginRequiredMixin,CreateView):
     template_name = 'accounts/registro.html'
     form_class = UserAdminCreationForm
     success_url = reverse_lazy('dashboard:dashboard')
+
+    def get_context_data(self, **kwargs):
+        context = super(RegisterView, self).get_context_data(**kwargs)
+        context['pgusuarios'] = 'active'
+        return context
+
+
 vwRegister = RegisterView.as_view()
